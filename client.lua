0b0de25d67834969e71ea580c68a0ff490b03067
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-04-11 16:34:35 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,12 @@
+-- Density values from 0.0 to 1.0.
+DensityMultiplier = 0.5
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+	    SetVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetPedDensityMultiplierThisFrame(DensityMultiplier)
+	    SetRandomVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetParkedVehicleDensityMultiplierThisFrame(DensityMultiplier)
+	    SetScenarioPedDensityMultiplierThisFrame(DensityMultiplier, DensityMultiplier)
+	end
+end)
---
+++ server.lua
@@ -0,0 +1,1 @@
+print("ReducedNPCs loaded! Made by man0s (https://github.com/man0s)")
